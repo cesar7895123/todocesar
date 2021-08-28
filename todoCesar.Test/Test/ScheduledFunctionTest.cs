718347692cdf758cc7f4b6aa8da5a4a54fdc8f20
cesar7895123 <cesar7895123@hotmail.com>
2021-08-28 00:57:32 -0500

    Add test: create todo, update todo and scheduledFunction

---
+++ todoCesar.Test/Test/ScheduledFunctionTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using todoCesar.Functions.Functions;
+using todoCesar.Test.Helpers;
+using Xunit;
+
+namespace todoCesar.Test.Test
+{
+    public class ScheduledFunctionTest
+    {
+
+        [Fact]
+        public void ScheduledFunction_Shoud_Log_Message()
+        {
+            // Arrange
+            MockCloudTableTodos mockTodos = new MockCloudTableTodos(new Uri("http://127.0.0.1:10002/devstoreacccount1/reports"));
+            ListLogger logger = (ListLogger)TestFactory.CreateLogger(LoggerTypes.List);
+
+            // Act
+            ScheduledFunction.Run(null, mockTodos, logger);
+            string message = logger.Logs[0];
+
+            // Assert
+            Assert.Contains("Deleting completed", message);
+        }
+    }
+}
