74cc2de8597054b5dc91c8fdc437db1b214dc681
cesar7895123 <cesar7895123@hotmail.com>
2021-08-27 20:17:31 -0500

    Add entities class

---
+++ todoCesar.Common/Responses/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace todoCesar.Common.Responses
+{
+    internal class Response
+    {
+        public bool IsSuccess { get; set; }
+
+        public string Message { get; set; }
+
+        public object Result { get; set; }
+    }
+}
