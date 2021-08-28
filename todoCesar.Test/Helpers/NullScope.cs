5531de71dcdce2165779da52a568ac87bcba64ec
cesar7895123 <cesar7895123@hotmail.com>
2021-08-28 00:15:57 -0500

    Unit test preparation

---
+++ todoCesar.Test/Helpers/NullScope.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace todoCesar.Test.Helpers
+{
+    public class NullScope : IDisposable
+    {
+        public static NullScope Instance { get; set; }
+        public void Dispose() { }
+
+        private NullScope() { }
+    }
+}
